68e5a38120b5cd7e1b2802c072a50b97fc6963d0
nisaooolle <@nisaoolle>
2023-11-01 10:07:55 +0700

    update

--- src/views/pages/informasi/PermintaanInformasi.js
+++ src/views/pages/informasi/PermintaanInformasi.js
@@ -1,110 +1,202 @@
-import React from 'react'
+import React from "react";
+import Navbar from "../../../component/Navbar";
 
 function PermintaanInformasi() {
   return (
-    <div class="blog-area pd-top-115 pd-bottom-60">
-    <div class="container">
-        <div class="row justify-content-center">
+    <>
+      <Navbar /> {/* <!-- page title start --> */}
+      <div class="breadcrumb-area bg-black bg-relative">
+        <div
+          class="banner-bg-img"
+          style={{
+            backgroundImage: `url('https://www.solverwp.com/demo/html/itechie/assets/img/bg/1.webp') `,
+          }}
+        ></div>
+        <div class="container">
+          <div class="row justify-content-center">
+            <div class="col-xl-7 col-lg-8">
+              <div class="breadcrumb-inner text-center">
+                <h2 style={{ color: "white", fontWeight: 700, fontSize: 60 }}>
+                  Tata Cara Permohonan Informasi
+                </h2>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* <!-- page title end --> */}
+      <div class="blog-area pd-top-115 pd-bottom-60">
+        <div class="container">
+          <div class="row justify-content-center">
             <div class="col-xl-6 col-lg-7 col-md-10">
-                <div class="section-title text-center">
-                    <h5 class="sub-title double-line">Blog Post</h5>
-                    <h2 class="title">Read Our Latest Tips &Tricks</h2>
-                    <p class="content">Dcidunt eget semper nec quam. Sed hendrerit. acfelis Nunc egestas augue
-                        atpellentesque laoreet</p>
-                </div>
+              <div class="section-title text-center">
+                <h5 class="sub-title double-line">Bawaslu Boyolali</h5>
+                <h2 class="title">Tata Cara Permohonan Informasi</h2>
+                {/* <p class="content">Dcidunt eget semper nec quam. Sed hendrerit. acfelis Nunc egestas augue
+                        atpellentesque laoreet</p> */}
+              </div>
             </div>
-        </div>
-        <div class="row justify-content-center">
+          </div>
+          <div class="row justify-content-center">
             <div class="col-lg-4 col-md-6">
-                <div class="single-blog-inner style-2">
-                    <div class="thumb">
-                        <img src="https://img.freepik.com/free-vector/public-talk-concept-illustration_114360-9552.jpg?w=740&t=st=1698730373~exp=1698730973~hmac=4e5084dda9f107a3af7af263cb00e6815278b83783f615a9745cb4bb8b2cf43f" alt="img"/>
-                    </div>
-                    <div class="details">
-                        <h4><a href="blog-details.html">Penyampaian Permohonan</a></h4>
-                        <ul class="blog-meta">
+              <div class="single-blog-inner style-2">
+                <div class="thumb">
+                  <img
+                    src="https://cdn.linkumkm.id/library/8/6/5/9/3/86593_840x576.jpg"
+                    alt="img"
+                  />
+                </div>
+                <div class="details">
+                  <h4>
+                    <a
+                      href="blog-details.html"
+                      style={{ textDecoration: "none", color: "black" }}
+                    >
+                      1) Penyampaian Permohonan
+                    </a>
+                  </h4>
+                  {/* <ul class="blog-meta">
                             <li><i class="far fa-user"></i>By Admin</li>
                             <li><i class="far fa-calendar-alt"></i> 25 May 2021</li>
-                        </ul>
-                        <p>Pemohon menyampaikan permohonan informasi kepada PPID melalui Aplikasi PPID,surat,fax,email,telepon atau datang langsung ke tempat layanan PPID</p>
-                        <a class="read-more-text" href="blog-details.html">Read More <i
-                                class="fa fa-caret-right"></i></a>
-                    </div>
+                        </ul> */}
+                  <p>
+                    Pemohon menyampaikan permohonan kepada PPID melalui Aplikasi
+                    PPID,surat,fax,email,telepon atau datang langsung ke tempat
+                    layanan PPID
+                  </p>
+                  {/* <a class="read-more-text" href="blog-details.html">Read More <i
+                                class="fa fa-caret-right"></i></a> */}
                 </div>
+              </div>
             </div>
             <div class="col-lg-4 col-md-6">
-                <div class="single-blog-inner style-2">
-                    <div class="thumb">
-                        <img src="https://img.freepik.com/free-vector/accept-terms-law-illustration-concept_114360-912.jpg?w=740&t=st=1698731008~exp=1698731608~hmac=209e72b4cc659d18ef9cdf9b7ddb8cbbe90b9376269c7d5dd09a3177ebcf9177" alt="img"/>
-                    </div>
-                    <div class="details">
-                        <h4><a href="blog-details.html">Mengisi Formulir</a></h4>
-                        <ul class="blog-meta">
+              <div class="single-blog-inner style-2">
+                <div class="thumb">
+                  <img
+                    src="https://static.promediateknologi.id/crop/0x0:0x0/750x500/webp/photo/2023/02/13/4001565610.jpg"
+                    alt="img"
+                  />
+                </div>
+                <div class="details">
+                  <h4>
+                    <a
+                      href="blog-details.html"
+                      style={{ textDecoration: "none", color: "black" }}
+                    >
+                      2) Mengisi Formulir
+                    </a>
+                  </h4>
+                  {/* <ul class="blog-meta">
                             <li><i class="far fa-user"></i> By Admin</li>
                             <li><i class="far fa-calendar-alt"></i> 25 May 2021</li>
-                        </ul>
-                        <p>Pemohon mengisi formulirmenyampaikan permohonan informasi dan memberikan salinan identitas diri/badan</p>
-                        <a class="read-more-text" href="blog-details.html">Read More <i
-                                class="fa fa-caret-right"></i></a>
-                    </div>
+                        </ul> */}
+                  <p>
+                    Pemohon mengisi formulir atau menyampaikan permohonan
+                    informasi dan memberikan salinan identitas diri atau badan
+                  </p>
+                  {/* <a class="read-more-text" href="blog-details.html">Read More <i
+                                class="fa fa-caret-right"></i></a> */}
                 </div>
+              </div>
             </div>
             <div class="col-lg-4 col-md-6">
-                <div class="single-blog-inner style-2">
-                    <div class="thumb">
-                        <img src="https://img.freepik.com/free-vector/grades-concept-illustration_114360-538.jpg?w=740&t=st=1698731149~exp=1698731749~hmac=8808c908261b05be751b1523b415df8ce3333c604745c5b5470df1949cf14f8c" alt="img"/>
-                    </div>
-                    <div class="details">
-                        <h4><a href="blog-details.html">Menerima Bukti</a></h4>
-                        <ul class="blog-meta">
+              <div class="single-blog-inner style-2">
+                <div class="thumb">
+                  <img
+                    src="https://cdn1.katadata.co.id/media/images/thumb/2021/12/15/Proposal_pengajuan_dana-2021_12_15-16_22_35_d15eba1331fd01144d06d67b279b8e7e_960x640_thumb.jpg"
+                    alt="img"
+                  />
+                </div>
+                <div class="details">
+                  <h4>
+                    <a
+                      href="blog-details.html"
+                      style={{ textDecoration: "none", color: "black" }}
+                    >
+                      3) Menerima Bukti
+                    </a>
+                  </h4>
+                  {/* <ul class="blog-meta">
                             <li><i class="far fa-user"></i> By Admin</li>
                             <li><i class="far fa-calendar-alt"></i> 25 May 2021</li>
-                        </ul>
-                        <p>Pemohon menerima bukti permohonan informasi dari petugas.Informasi apabila syarat permohonan telah di lengkapi </p>
-                        <a class="read-more-text" href="blog-details.html">Read More <i
-                                class="fa fa-caret-right"></i></a>
-                    </div>
+                        </ul> */}
+                  <p>
+                    Pemohon menerima bukti permohonan informasi dari
+                    petugas.Informasi apabila syarat permohonan telah di
+                    lengkapi{" "}
+                  </p>
+                  {/* <a class="read-more-text" href="blog-details.html">Read More <i
+                                class="fa fa-caret-right"></i></a> */}
                 </div>
+              </div>
             </div>
-        </div>
-        <div class="row justify-content-center">
+          </div>
+          <div class="row justify-content-center">
             <div class="col-lg-4 col-md-6">
-                <div class="single-blog-inner style-2">
-                    <div class="thumb">
-                        <img src="https://img.freepik.com/free-vector/career-orientation-concept-illustration_114360-19870.jpg?w=740&t=st=1698731583~exp=1698732183~hmac=ecdb31f445f7b4aaebbdfc2e329e50b3a9e954c463bb633bdb7f585ac009eec6" alt="img"/>
-                    </div>
-                    <div class="details">
-                        <h4><a href="blog-details.html">Jangka Waktu</a></h4>
-                        <ul class="blog-meta">
+              <div class="single-blog-inner style-2">
+                <div class="thumb">
+                  <img
+                    src="https://pasardana.id/media/11420/ilustrasi-waktu-kerja.jpg?crop=0,0,0,0.15488054001243451&cropmode=percentage&width=675&height=380&rnd=132016261740000000"
+                    alt="img"
+                  />
+                </div>
+                <div class="details">
+                  <h4>
+                    <a
+                      href="blog-details.html"
+                      style={{ textDecoration: "none", color: "black" }}
+                    >
+                      4) Jangka Waktu
+                    </a>
+                  </h4>
+                  {/* <ul class="blog-meta">
                             <li><i class="far fa-user"></i>By Admin</li>
                             <li><i class="far fa-calendar-alt"></i> 25 May 2021</li>
-                        </ul>
-                        <p>Dalam jangka waktu 10 hari kerja, pemohon menerima pemberitahuan tertulis dari PPID</p>
-                        <a class="read-more-text" href="blog-details.html">Read More <i
-                                class="fa fa-caret-right"></i></a>
-                    </div>
+                        </ul> */}
+                  <p>
+                    Dalam jangka waktu 10 hari kerja, pemohon menerima
+                    pemberitahuan tertulis dari PPID
+                  </p>
+                  {/* <a class="read-more-text" href="blog-details.html">Read More <i
+                                class="fa fa-caret-right"></i></a> */}
                 </div>
+              </div>
             </div>
             <div class="col-lg-4 col-md-6">
-                <div class="single-blog-inner style-2">
-                    <div class="thumb">
-                        <img src="https://img.freepik.com/free-vector/mailbox-concept-illustration_114360-234.jpg?w=740&t=st=1698731450~exp=1698732050~hmac=69b011b44b29e46567f4d3cad5b95200776f2b98d429811f320e5b81ab994bb3" alt="img"/>
-                    </div>
-                    <div class="details">
-                        <h4><a href="blog-details.html">Menerima Informasi</a></h4>
-                        <ul class="blog-meta">
+              <div class="single-blog-inner style-2">
+                <div class="thumb">
+                  <img
+                    src="https://cdn0-production-images-kly.akamaized.net/MJN9I-eHhc9eqIzLsqc0-RmOHvk=/800x450/smart/filters:quality(75):strip_icc():format(webp)/kly-media-production/medias/4138775/original/064259700_1661740032-close-up-man-sealing-envelope.jpg"
+                    alt="img"
+                  />
+                </div>
+                <div class="details">
+                  <h4>
+                    <a
+                      href="blog-details.html"
+                      style={{ textDecoration: "none", color: "black" }}
+                    >
+                      5) Menerima Informasi
+                    </a>
+                  </h4>
+                  {/* <ul class="blog-meta">
                             <li><i class="far fa-user"></i> By Admin</li>
                             <li><i class="far fa-calendar-alt"></i> 25 May 2021</li>
-                        </ul>
-                        <p>Pemohon informasi menerima informasi yang diminta atau Surat Keputusan PPID tentang penolakan permohonan informasi dari petugas.</p>
-                        <a class="read-more-text" href="blog-details.html">Read More <i
-                                class="fa fa-caret-right"></i></a>
-                    </div>
+                        </ul> */}
+                  <p>
+                    Pemohon menerima informasi Surat Keputusan PPID tentang
+                    penolakan permohonan informasi dari petugas.
+                  </p>
+                  {/* <a class="read-more-text" href="blog-details.html">Read More <i
+                                class="fa fa-caret-right"></i></a> */}
                 </div>
+              </div>
             </div>
+          </div>
         </div>
-    </div>
-</div>  )
+      </div>
+    </>
+  );
 }
 
-export default PermintaanInformasi
+export default PermintaanInformasi;
