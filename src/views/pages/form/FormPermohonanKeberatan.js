0e0ccd127423886438196484aaa853721f2c8475
fatiyasalsabila <fatiyasalsabila83@gmail.com>
2023-11-10 07:22:23 +0700

    update

--- src/views/pages/form/FormPermohonanKeberatan.js
+++ src/views/pages/form/FormPermohonanKeberatan.js
@@ -8,8 +8,63 @@ import jateng from "../../../aset/bawaslu-jateng-300x73-1.png";
 import dkpp from "../../../aset/dkpp-300x73-1.png";
 import KPU from "../../../aset/KPU-300x73-1.png";
 import MAHKAMAH from "../../../aset/MAHKAMAKONSTITUSI-300x73-1.png";
+import { useState } from "react";
+import { API_DUMMY } from "../../../utils/base_URL";
+import axios from "axios";
+import Swal from "sweetalert2";
 
 const FormPermohonaKeberatan = () => {
+  const [namaPermohonan, setNamaPermohonan] = useState("");
+  const [kasusPosisi, setKasusPosisi] = useState("");
+  const [noHp, setNoHp] = useState("");
+  const [email, setEmail] = useState("");
+  const [jenisIdentitas, setJenisIdentitas] = useState("");
+  const [namaPenggunaInformasi, setNamaPenggunaInformasi] = useState("");
+  const [alamat, setAlamat] = useState("");
+  const [alasan, setAlasan] = useState("");
+  const [nomorIdentitas, setNomorIdentitas] = useState("");
+  const [file, setFile] = useState("");
+  const [tujuanPenggunaanInformasi, setTujuanPenggunaanInformasi] = useState("");
+  const [show, setShow] = useState(false);
+
+  const add = async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData();
+    formData.append("namaPermohonan", namaPermohonan);
+    formData.append("kasusPolisi", kasusPosisi);
+    formData.append("email", email);
+    formData.append("noHp", noHp);
+    formData.append("jenisIdentitas", jenisIdentitas);
+    formData.append("namaPenggunaInformasi", namaPenggunaInformasi);
+    formData.append("alamat", alamat);
+    formData.append("alasan", alasan);
+    formData.append("nomorIdentitas", nomorIdentitas);
+    formData.append("file", file); // Upload gambar jika dipilih
+
+    try {
+      await axios.post(
+        `${API_DUMMY}/bawaslu/api/permohonan-keberatan/add`,
+        formData,
+        {
+          headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
+        }
+      );
+
+      setShow(false);
+      Swal.fire({
+        icon: "success",
+        title: "Berhasil Ditambahkan",
+        showConfirmButton: false,
+        timer: 1500,
+      });
+      setTimeout(() => {
+        window.location.reload();
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <>
       <div>
@@ -31,44 +86,51 @@ const FormPermohonaKeberatan = () => {
             className="option"
           >
             <div>
-              <form>
+              <form onSubmit={add}>
                 <p >Nama Pemohon:</p>
-                <input className="input" />
+                <input className="input" type="text" value={namaPermohonan} onChange={(e) => setNamaPermohonan(e.target.value)} />
                 <p className="pp" >0 of 30 max characters</p>
                 <br></br>
                 <p>Alamat Pemohon:</p>
-                <textarea className="textarea"  />
+                <textarea className="textarea" value={alamat} onChange={(e) => setAlamat(e.target.value)}   />
                 <p className="pp" >0 of 60 max characters</p>
                 <br></br>
-                <p>Nomor Identitas Pemohon:</p>
-                <input className="input" />
+                <p>Jenis Identitas Pemohon:</p>
+                <input className="input" type="text" value={jenisIdentitas} onChange={(e) => setJenisIdentitas(e.target.value)}  />
                 <p className="pp" >0 of 30 max characters</p>
                 <br></br>
-                <p>Jenis Identitas:</p>
+                <p>Nomor Identitas Pemohon:</p>
+                <input className="input" type="number" value={nomorIdentitas} onChange={(e) => setNomorIdentitas(e.target.value)}  />
+                <p className="pp" >0 of 30 max characters</p>
+                <br></br>
+                {/* <p>Jenis Identitas:</p>
                 <select className="select" >
                   <option>KTP (Kartu Tanda Penduduk)</option>
                   <option>SIM (Surat Izin Mengemudi)</option>
                   <option>KTM (Kartu Tanda Mahasiswa)</option>
-                </select>
+                </select> */}
                 <br></br>
                 <br></br>
                 <p>Nomor Telp/HP:</p>
-                <input className="input"  />
+                <input className="input" type="number" value={noHp} onChange={(e) => setNoHp(e.target.value)}   />
                 <p className="pp" >0 of 30 max characters</p>
                 <br></br>
                 <p>Email:</p>
-                <textarea className="textarea"  />
+                <textarea className="textarea" type="email" value={email} onChange={(e) => setEmail(e.target.value)}   />
                 <p className="pp" >0 of 30 max characters</p>
                 <br></br>
                 <p>Kasus Posisi:</p>
-                <textarea className="textarea"  />
+                <textarea className="textarea" type="text" value={kasusPosisi} onChange={(e) => setKasusPosisi(e.target.value)}   />
                 <p className="pp" >0 of 100 max characters</p>
                 <br></br>
                 <p>Tujuan Penggunaan Informasi:</p>
-                <textarea className="textarea"  />
+                <textarea className="textarea" value={tujuanPenggunaanInformasi} onChange={(e) => setTujuanPenggunaanInformasi(e.target.value)}   />
                 <p className="pp" >0 of 100 max characters</p>
                 <br></br>
-                <select className="select"  >
+                <p>Alasan</p>
+                <textarea className="textarea" value={alasan} onChange={(e) => setAlasan(e.target.value)}   />
+                <p className="pp" >0 of 100 max characters</p>
+                {/* <select className="select"  >
                   <option>Permohonan Informasi Ditolak</option>
                   <option>Informasi Berkala Tidak Disediakan</option>
                   <option>Permintaan Informasi Tidak Ditanggapi</option>
@@ -81,7 +143,7 @@ const FormPermohonaKeberatan = () => {
                     Informasi Disampaikan Pada Jangka Waktu Yang Sudah
                     Ditentukan
                   </option>
-                </select>
+                </select> */}
                 <br></br>
                 <br></br>
                 <p>Upload Photo Identitas (.jpg) size max 1mb </p>
@@ -91,11 +153,11 @@ const FormPermohonaKeberatan = () => {
                 >
                   {" "}
                   <div>
-                    <input type="file" />
+                    <input type="file" onChange={(e) => setFile(e.target.files[0])}  />
                   </div>
                   <div>
                     {" "}
-                    <button
+                    <button type="submit"
                     className="btn-kirim"
                      
                     >
