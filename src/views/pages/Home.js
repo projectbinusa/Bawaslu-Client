ab07521dff6e5465e5917312c52a9492037fc69c
dzakiyaalfyatunnufus02 <dzakiyaalfyatunnufus@gmail.com>
2023-12-22 16:46:18 +0700

    update

--- src/views/pages/Home.js
+++ src/views/pages/Home.js
@@ -123,6 +123,7 @@ function Home() {
     },
     { title: "Yeeze", image: "images/yeeze.jpg", bgColor: "#D7A594" },
   ];
+
   const swiper = useRef(null);
   const currentSlideIndex = useRef(0);
 
@@ -229,6 +230,15 @@ function Home() {
               modifier: 2.5,
             }}
             className="swiper_container"
+            navigation={{
+              nextEl: '.swiper-button-next',
+              prevEl: '.swiper-button-prev',}}
+              pagination={{
+                el: '.swiper-pagination',
+                clickable: true,
+              }}
+              onSwiper={(swiperInstance) => (swiper.current = swiperInstance)}
+         
           >
             <SwiperSlide>
               <img
@@ -284,14 +294,20 @@ function Home() {
             </div> */}
           </Swiper>
           <div className="slider-controler">
-        <div className="swiper-button-prev slider-arrow" onClick={handlePrev}>
-          <ion-icon name="arrow-back-outline"></ion-icon>
-        </div>
-        <div className="swiper-button-next slider-arrow" onClick={handleNext}>
-          <ion-icon name="arrow-forward-outline"></ion-icon>
-        </div>
-        <div className="swiper-pagination"></div>
-      </div>
+            <div
+              className="swiper-button-prev slider-arrow"
+              onClick={handlePrev}
+            >
+              <ion-icon name="arrow-back-outline"></ion-icon>
+            </div>
+            <div
+              className="swiper-button-next slider-arrow"
+              onClick={handleNext}
+            >
+              <ion-icon name="arrow-forward-outline"></ion-icon>
+            </div>
+            <div className="swiper-pagination"></div>
+          </div>
         </div>
         {/* intro area end */}
         <div
@@ -684,7 +700,6 @@ function Home() {
       <Footer />
     </div>
   );
-  
 }
 
 export default Home;
